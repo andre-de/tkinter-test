40fffc0714ad4b6097cdc7db070bf3d122775801
andre-de <neubertandre@yahoo.de>
2025-02-10 16:16:19 +0100

    save

--- src/main.py
+++ src/main.py
@@ -3,26 +3,27 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.__root = Tk()
-        self.__root.title("Title")
-        self.canvas = Canvas(self.__root)
-        self.canvas.pack()
-        self.running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self._width = width
+        self._height = height
+        self._root = Tk()
+        self._root.title("Title")
+        self._root.geometry(str(width)+"x"+str(height))
+        self._canvas = Canvas(self._root)
+        self._canvas.pack(fill=BOTH, expand=True)
+        self._running = False
+        self._root.protocol("WM_DELETE_WINDOW", self.close)
     
     def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
+        self._root.update_idletasks()
+        self._root.update()
     
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self._running = True
+        while self._running:
             self.redraw()
     
     def close(self):
-        self.running = False
+        self._running = False
 
 
 
