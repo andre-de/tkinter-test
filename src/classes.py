60bae12c07209d5539320e941214219621bd562a
andre-de <neubertandre@yahoo.de>
2025-02-11 09:56:34 +0100

    save

--- src/classes.py
+++ src/classes.py
@@ -41,3 +41,38 @@ class Line:
     def draw(self, canvas, fill_color):
         canvas.create_line(self._p1.x, self._p1.y, self._p2.x, self._p2.y, fill=fill_color, width = 2)
 
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._xtlc = None
+        self._ytlc = None
+        self._xbrc = None
+        self._ybrc = None
+        self._win = win
+
+    def draw(self, xtlc, ytlc, xbrc, ybrc):
+        self._xtlc = xtlc
+        self._ytlc = ytlc
+        self._xbrc = xbrc
+        self._ybrc = ybrc
+
+        if self.has_left_wall:
+            left_wall = Line(Point(self._xtlc, self._ytlc), Point(self._xtlc, self._ybrc))
+            self._win.draw_line(left_wall, "black")
+
+        if self.has_right_wall:
+            right_wall = Line(Point(self._xbrc, self._ytlc), Point(self._xbrc, self._ybrc))
+            self._win.draw_line(right_wall, "black")
+
+        if self.has_top_wall:
+            top_wall = Line(Point(self._xtlc, self._ytlc), Point(self._xbrc, self._ytlc))
+            self._win.draw_line(top_wall, "black")
+
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(self._xtlc, self._ybrc), Point(self._xbrc, self._ybrc))
+            self._win.draw_line(bottom_wall, "black")
+
+    
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-from classes import Window, Point, Line
+from classes import Window, Point, Line, Cell
 
 def main():
     win = Window(800, 600)
 
+    '''
     point1 = Point(100, 100)
     point2 = Point(200, 200)
     line1 = Line(point1, point2)
@@ -12,7 +13,18 @@ def main():
     point4 = Point(10, 500)
     line2 = Line(point3, point4)
     win.draw_line(line2, "red")
+    '''
+    cell1 = Cell(win)
+    cell1.draw(100, 100, 200, 200)
 
+    cell2 = Cell(win)
+    cell2.has_left_wall = False
+    cell2.draw(300, 100, 400, 200)
+
+    cell3 = Cell(win)
+    cell3.has_top_wall = False
+    cell3.has_bottom_wall = False
+    cell3.draw(500, 100, 600, 200)
     
     win.wait_for_close()
 
